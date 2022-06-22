438d0911db0248b0a1c8a5dd06550c2984e63ac1
Ifzy keys <johnsonnifemi8@gmail.com>
2022-06-22 21:37:03 +0100

    add script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const hamBurger = document.querySelector('.hamburger-container');
 const closeBtn = document.querySelector('#closeBtn');
-const hamBurgerBtn = document.querySelector ('#hamburger');
+const hamBurgerBtn = document.querySelector('#hamburger');
 const menuItem1 = document.querySelector('#menuabout');
 const menuItem2 = document.querySelector('#program');
 const menuItem3 = document.querySelector('#join');
 const menuItem4 = document.querySelector('#Sponsor');
 const menuItem5 = document.querySelector('#enlist');
-function openMenu () {
-    hamBurger.classList.toggle('extended');
+function openMenu() {
+  hamBurger.classList.toggle('extended');
 }
-function closeMenu () {
-    hamBurger.classList.remove('extended');
+function closeMenu() {
+  hamBurger.classList.remove('extended');
 }
 hamBurgerBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
@@ -26,28 +26,28 @@ const featuring1 = {
   image: 'images/Cory_Henry.png',
   name: 'Cory henry and the funk apostles',
   description: 'A musical ban d',
-  overview: 'Cory Alexander Henry is an American singer-songwriter, pianist and producer.'
+  overview: 'Cory Alexander Henry is an American singer-songwriter, pianist and producer.',
 };
 const featuring2 = {
   image: 'images/Richard_Bona_in_2009.jpg',
   name: 'Richard Bona',
   description: 'A bass player',
-  overview: 'Richard Bona (born 28 October 1967) is a Cameroon-born American musician'
+  overview: 'Richard Bona (born 28 October 1967) is a Cameroon-born American musician',
 };
 const featuring3 = {
   image: 'images/michaelleague_5050.jpg',
   name: 'Snarky Puppy',
   description: 'A musical band',
-  overview: 'Snarky Puppy is an American instrumental ensemble led by bassist  Michael League.'
+  overview: 'Snarky Puppy is an American instrumental ensemble led by bassist  Michael League.',
 };
 const featuring4 = {
   image: 'images/stanleyrandolph730.jpg',
   name: 'Stanley Randolph',
   description: 'A drummer',
-  overview: 'Stanley Randolph continues to amaze the drumming world with his quick hands.'
+  overview: 'Stanley Randolph continues to amaze the drumming world with his quick hands.',
 };
 
-function coryHenry (){
+function coryHenry() {
   document.querySelector('.cimage').src = `${featuring1.image}`;
   document.querySelector('.cname').innerHTML= featuring1.name;
   document.querySelector('.cdescription').innerHTML = featuring1.description;
@@ -55,21 +55,21 @@ function coryHenry (){
 }
 
 
-function richardBona () {
+function richardBona() {
   document.querySelector('.rimage').src = `${featuring2.image}`;
   document.querySelector('.rname').innerHTML = featuring2.name;
   document.querySelector('.rdescription').innerHTML = featuring2.description;
   document.querySelector('.roverview').innerHTML = featuring2.overview;
 }
 
-function snarkyPuppy (){
+function snarkyPuppy() {
   document.querySelector('.simage').src = `${featuring3.image}`;
   document.querySelector('.sname').innerHTML = featuring3.name;
   document.querySelector('.sdescription').innerHTML = featuring3.description;
   document.querySelector('.soverview').innerHTML = featuring3.overview;
 }
 
-function stanleyRandolph (){
+function stanleyRandolph() {
   document.querySelector('.stimage').src = `${featuring4.image}`;
   document.querySelector('.stname').innerHTML = featuring4.name;
   document.querySelector('.stdescription').innerHTML = featuring4.description;
