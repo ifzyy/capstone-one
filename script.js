12aaddbd534f894badc43deee0450d798ddd5d4d
Ifzy keys <johnsonnifemi8@gmail.com>
2022-06-23 15:17:12 +0100

    update script

--- script.js
+++ script.js
@@ -21,30 +21,30 @@ menuItem3.addEventListener('click', closeMenu);
 menuItem4.addEventListener('click', closeMenu);
 menuItem5.addEventListener('click', closeMenu);
 
-const featuring1 = {
+const featuring = [{
   image: 'images/Cory_Henry.png',
   name: 'Cory henry and the funk apostles',
   description: 'A musical band',
   overview: 'Cory Alexander Henry is an American singer-songwriter, pianist and producer.',
-};
-const featuring2 = {
+},
+ {
   image: 'images/Richard_Bona_in_2009.jpg',
   name: 'Richard Bona',
   description: 'A bass player',
   overview: 'Richard Bona (born 28 October 1967) is a Cameroon-born American musician',
-};
-const featuring3 = {
+},
+{
   image: 'images/michaelleague_5050.jpg',
   name: 'Snarky Puppy',
   description: 'A musical band',
   overview: 'Snarky Puppy is an American instrumental ensemble led by bassist  Michael League.',
-};
-const featuring4 = {
+},
+ {
   image: 'images/stanleyrandolph730.jpg',
   name: 'Stanley Randolph',
   description: 'A drummer',
   overview: 'Stanley Randolph continues to amaze the drumming world with his quick hands.',
-};
+}];
 
 const container = document.querySelector('.featured-speaker-container');
 const h3 = document.createElement('h3');
@@ -156,31 +156,31 @@ featureBoxText2st.classList.add('feature-box-text2', 'stoverview');
 featureBoxContent4.appendChild(featureBoxText2st);
 
 function coryHenry() {
-  document.querySelector('.cimage').src = `${featuring1.image}`;
-  document.querySelector('.cname').innerHTML = featuring1.name;
-  document.querySelector('.cdescription').innerHTML = featuring1.description;
-  document.querySelector('.coverview').innerHTML = featuring1.overview;
+  document.querySelector('.cimage').src = `${featuring[0].image}`;
+  document.querySelector('.cname').innerHTML = featuring[0].name;
+  document.querySelector('.cdescription').innerHTML = featuring[0].description;
+  document.querySelector('.coverview').innerHTML = featuring[0].overview;
 }
 
 function richardBona() {
-  document.querySelector('.rimage').src = `${featuring2.image}`;
-  document.querySelector('.rname').innerHTML = featuring2.name;
-  document.querySelector('.rdescription').innerHTML = featuring2.description;
-  document.querySelector('.roverview').innerHTML = featuring2.overview;
+  document.querySelector('.rimage').src = `${featuring[1].image}`;
+  document.querySelector('.rname').innerHTML = featuring[1].name;
+  document.querySelector('.rdescription').innerHTML = featuring[1].description;
+  document.querySelector('.roverview').innerHTML = featuring[1].overview;
 }
 
 function snarkyPuppy() {
-  document.querySelector('.simage').src = `${featuring3.image}`;
-  document.querySelector('.sname').innerHTML = featuring3.name;
-  document.querySelector('.sdescription').innerHTML = featuring3.description;
-  document.querySelector('.soverview').innerHTML = featuring3.overview;
+  document.querySelector('.simage').src = `${featuring[2].image}`;
+  document.querySelector('.sname').innerHTML = featuring[2].name;
+  document.querySelector('.sdescription').innerHTML = featuring[2].description;
+  document.querySelector('.soverview').innerHTML = featuring[2].overview;
 }
 
 function stanleyRandolph() {
-  document.querySelector('.stimage').src = `${featuring4.image}`;
-  document.querySelector('.stname').innerHTML = featuring4.name;
-  document.querySelector('.stdescription').innerHTML = featuring4.description;
-  document.querySelector('.stoverview').innerHTML = featuring4.overview;
+  document.querySelector('.stimage').src = `${featuring[3].image}`;
+  document.querySelector('.stname').innerHTML = featuring[3].name;
+  document.querySelector('.stdescription').innerHTML = featuring[3].description;
+  document.querySelector('.stoverview').innerHTML = featuring[3].overview;
 }
 
 coryHenry();
